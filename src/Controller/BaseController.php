9624e1826aad9ebcfed1f6511a4d4cf5bbfbd1f8
Nicolas <nicolas.legendre.ter@gmail.com>
2019-08-19 17:12:45 +0200

    lien template-pages

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class BaseController
- * @Route("/Base/")
+ * @Route("/Main/")
  *
  * @package App\Controller
  */
@@ -17,11 +17,83 @@ class BaseController extends Controller
 {
 
     /**
-     * @Route("Test")
+     * @Route("", name="main_accueil", methods={"GET"})
      */
-    public function showBase(){
+    public function accueil(){
 
-        return $this->render("base.html.twig");
+        return $this->render("Accueil.html.twig");
+    }
+
+    /**
+     * @Route("Recherche", name="main_recherche", methods={"GET"})
+     */
+    public function recherche(){
+
+        return $this->render("Recherche.html.twig");
+
+    }
+
+    /**
+     * @Route("Annonce", name="main_annonce", methods={"GET"})
+     */
+    public function annonce(){
+
+        return $this->render("Annonce.html.twig");
+
+    }
+
+    /**
+     * @Route("CGU", name="main_CGU", methods={"GET"})
+     */
+    public function CGU(){
+
+        return $this->render("CGU.html.twig");
+
+    }
+
+    /**
+     * @Route("Connexion", name="main_connexion", methods={"GET"})
+     */
+    public function connexion(){
+
+        return $this->render("Connexion.html.twig");
+
+    }
+
+    /**
+     * @Route("Deposer", name="main_deposer", methods={"GET"})
+     */
+    public function deposer(){
+
+        return $this->render("Deposer.html.twig");
+
+    }
+
+    /**
+     * @Route("FAQ", name="main_FAQ", methods={"GET"})
+     */
+    public function FAQ(){
+
+        return $this->render("FAQ.html.twig");
+
+    }
+
+    /**
+     * @Route("MesAnnonces", name="main_mesAnnonces", methods={"GET"})
+     */
+    public function mesAnnonces(){
+
+        return $this->render("MesAnnonces.html.twig");
+
+    }
+
+    /**
+     * @Route("MesFavoris", name="main_mesFavoris", methods={"GET"})
+     */
+    public function mesFavoris(){
+
+        return $this->render("MesFavoris.html.twig");
+
     }
 
 }
